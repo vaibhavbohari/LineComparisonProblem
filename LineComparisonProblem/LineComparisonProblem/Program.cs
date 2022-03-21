e29414cab1210ce3a7c9a301276d3630018d96ef
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-21 20:29:32 +0530

    Length Of Line

--- LineComparisonProblem/LineComparisonProblem/Program.cs
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Line Comparison Problem");
+﻿
+using System;
+
+namespace LineComparisonProblem
+{
+    public class LineComparision
+    {
+        ///variables
+        public double length_Of_Line;
+        ///method for calculation
+        public void Calc(double x1, double y1, double x2, double y2)
+        {
+            double X = Math.Pow(x2 - x1, 2.0);
+            double Y = Math.Pow(y2 - y1, 2.0);
+            length_Of_Line = Math.Sqrt(X + Y);
+        }
+    }
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            LineComparision lc = new LineComparision();
+            Console.WriteLine("Welcome To Line Comparison Problem ");
+            Console.WriteLine("enter x1,y1,x2,y2 values\n");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            lc.Calc(a, b, c, d);
+            Console.WriteLine("Line Length is = " + lc.length_Of_Line);
+        }
+
+    }
+}
